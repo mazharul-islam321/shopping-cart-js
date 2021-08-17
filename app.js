bfcae10a922c769f43c534d58ea534720361a2a6
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2021-08-18 03:19:21 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// product number function
+function updateProductNumber() {
+    console.log();
+}
+
+document.getElementById("case-plus").addEventListener("click", function () {
+    updateCaseNumber();
+});
+document.getElementById("case-minus").addEventListener("click", function () {
+    updateCaseNumber();
+});
